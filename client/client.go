a81bad62407370295d8ea0868fef292486391477
sideOfBeans <dcondr2@illinois.edu>
2024-09-07 14:46:46 -0500

    added logs and function to read out lines of log files with line numbers

--- client/client.go
+++ client/client.go
@@ -13,6 +13,14 @@ type result struct {
 	Test_Int int
 }
 
+type Grep_Result struct {
+	Matches []string
+}
+
+type Grep_Args struct {
+	Line_Args []string
+}
+
 func main() {
 	reply := result{}
 	args := Args{1, 2, 3}
@@ -25,4 +33,12 @@ func main() {
 		log.Fatal("client call failed:", err)
 	}
 	log.Printf("sum of 3 numbers: %d", reply.Test_Int)
+
+	// grep start
+	grep_reply := Grep_Result{}
+	grep_args := Grep_Args{make([]string, 5)} // testing []string
+	err = client.Call("Query.Grep", grep_args, &grep_reply)
+	if err != nil {
+		log.Fatal("client call failed:", err)
+	}
 }
--- server/server.go
+++ server/server.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"net/rpc"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
 )
 
 type Result struct {
@@ -26,9 +32,70 @@ func (t *Arith) Add3(args *Args, result *Result) error {
 	return nil
 }
 
+type Grep_Result struct {
+	Matches []string
+}
+
+type Grep_Args struct {
+	Line_Args []string
+}
+
+type Query int
+
+func (t *Query) Grep(args *Grep_Args, result *Grep_Result) error {
+	if len(args.Line_Args) == 0 {
+		return errors.New("no arguments passed to grep")
+	}
+	err := os.Chdir("./logs")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var curr_dir string
+	curr_dir, err = os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// iterate
+
+	filepath.Walk(curr_dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Fatal(err)
+		}
+		// TODO: change to grep files instead of just printing the file name
+		if !info.IsDir() { // walk includes the root so make sure to exclude that
+			var f *os.File
+
+			f, err = os.Open(path)
+			if err != nil {
+				log.Fatal(err)
+			}
+			line_number := 1
+
+			file_scanner := bufio.NewScanner(f)
+			file_scanner.Split(bufio.ScanLines) // there might be a limit on the line length to check for
+
+			for file_scanner.Scan() {
+				fmt.Println(strconv.Itoa(line_number) + ": " + file_scanner.Text())
+				line_number++
+			}
+			fmt.Printf("File name: %s\n", info.Name())
+			err = f.Close()
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		return nil
+	})
+	// TODO: modify to grep the file in the Walk function
+	exec.Command("grep")
+	return nil
+}
+
 func main() {
 	arith := new(Arith)
 	rpc.Register(arith)
+	grep := new(Query)
+	rpc.Register(grep)
 	rpc.HandleHTTP()
 	l, err := net.Listen("tcp", ":8880")
 	if err != nil {
