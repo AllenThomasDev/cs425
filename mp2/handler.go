3ac57dbfe9ffde52f98de04e172fd482e76dfc89
Allen Thomas <allen.thomas.dev@gmail.com>
2024-10-16 17:15:50 -0500

    seperate out command listener and handler

---
+++ mp2/handler.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func commandListener() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("Enter command: ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading input:", err)
+			continue
+		}
+		input = strings.TrimSpace(input)
+		switch input {
+		case "list_mem":
+			listMembership()
+		case "list_self":
+			listSelf()
+		case "join":
+			joinGroup()
+		case "leave":
+			leaveGroup()
+		case "enable_sus":
+			enableSuspicion()
+		case "disable_sus":
+			disableSuspicion()
+		case "list_sus":
+			listSuspectedNodes()
+		case "status_sus":
+			statusSuspicion()
+		default:
+			fmt.Println("Unknown command")
+		}
+	}
+}
+func handleMessage(msg string) {
+	parts := strings.Split(msg, ",")
+	command := parts[0]
+	switch command {
+	case "SUS_OFF":
+		suspicionEnabled = false
+	case "SUS_ON":
+		suspicionEnabled = true
+	// Only the introducer can ingest JOIN messages
+	case "JOIN":
+		sender_ip := parts[1]
+		ts := parts[2]
+		inc := parts[3]
+		_, exists := checkMembershipList(selfIP)
+		if !exists {
+			fmt.Print("someone tried to join the group but i am not here")
+			return
+		}
+		addMember(sender_ip, ts, inc)
+		sendToAll(fmt.Sprintf("NEW_MEMBER,%s,%s,%s", sender_ip, ts, inc))
+		sendFullMembershipList(sender_ip)
+		if suspicionEnabled {
+			sus_status := "SUS_ON"
+			sendMessageViaTCP(sender_ip, sus_status)
+		} else {
+			sus_status := "SUS_OFF"
+			sendMessageViaTCP(sender_ip, sus_status)
+		}
+	case "NEW_MEMBER":
+		sender_ip := parts[1]
+		ts := parts[2]
+		inc := parts[3]
+		addMember(sender_ip, ts, inc)
+	case "LEAVE":
+		leaver_ip := parts[1]
+		logger.Printf("Received LEAVE message from %s", leaver_ip)
+		if leaver_ip == selfIP {
+			fmt.Printf("I was killed unfortunately, if only you had enabled suspicion earlier\n\n\n :(")
+			os.Exit(0)
+		}
+		removeMember(leaver_ip)
+	case "PING":
+		senderIP := parts[1]
+		// Send ACK back to the sender
+		sendMessageViaTCP(senderIP, fmt.Sprintf("ACK,%s", selfIP))
+	case "ACK":
+		senderIP := parts[1]
+		timestamp := time.Now().Unix()
+		updateLastAckReceived(senderIP, timestamp)
+		removeNodeFromSuspected(senderIP)
+	case "SUSPECT":
+		if !suspicionEnabled {
+			return
+		}
+		suspectedIP := parts[1]
+		suspectedIncarnation, _ := strconv.ParseInt(parts[2], 10, 64)
+		if suspectedIP == selfIP {
+			if suspectedIncarnation >= getIncarnationNumber() {
+				incrementIncarnation()
+				sendRefutation()
+			}
+		} else {
+			updateMemberIncarnation(suspectedIP, suspectedIncarnation)
+			markNodeAsSuspected(suspectedIP)
+			startSuspicionTimer(suspectedIP)
+		}
+		listSuspectedNodes()
+
+	case "REFUTE":
+		if !suspicionEnabled {
+			return
+		}
+		refutingIP := parts[1]
+		refuteIncarnation, _ := strconv.ParseInt(parts[2], 10, 64)
+		updateMemberIncarnation(refutingIP, refuteIncarnation)
+		removeNodeFromSuspected(refutingIP)
+	}
+}
