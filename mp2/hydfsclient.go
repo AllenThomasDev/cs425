ca70e4486c622f97e9d92b4cbb09c2dee55a31b1
Allen Thomas <allen.thomas.dev@gmail.com>
2024-11-07 14:41:52 -0600

    fix not setting cache flag

--- mp2/hydfsclient.go
+++ mp2/hydfsclient.go
@@ -271,8 +271,8 @@ func commandListener() {
 					logger.Printf("File content saved successfully to %s from cache\n", localFilename)
 					fmt.Println("GET (from cache) completed")
 					duration := time.Since(startTime)
-					logger.Printf("GET took %s, cache status %v \n", duration, servedFromCache)
 					servedFromCache = true
+					logger.Printf("GET took %s, cache status %v \n", duration, servedFromCache)
 				}
 				continue
 			}
