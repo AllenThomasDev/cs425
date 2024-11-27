81806b3363c80c981745e548fe534f422c45c02e
Allen Thomas <allen.thomas.dev@gmail.com>
2024-11-27 16:58:32 -0600

    create some operators and tests

---
+++ mp3/operators.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+var wordCounts = make(map[string]int)
+
+func splitLine(rt Rainstorm_tuple_t) []Rainstorm_tuple_t {
+	words := strings.Fields(rt.Value)
+	var tuples []Rainstorm_tuple_t
+	for _, word := range words {
+		tuples = append(tuples, Rainstorm_tuple_t{
+			Key:   word,            // Append the index to the key to make each tuple unique
+			Value: strconv.Itoa(1), // Assign each word to a new tuple
+		})
+	}
+	return tuples
+}
+
+func wordCountOperator(rt Rainstorm_tuple_t) Rainstorm_tuple_t {
+	wordCounts[rt.Key]++
+	fmt.Printf("%s Count: %s\n", rt.Key, strconv.Itoa(wordCounts[rt.Key]))
+  return Rainstorm_tuple_t{
+		Key:   rt.Key,
+		Value: strconv.Itoa(wordCounts[rt.Key]), // Running count as string
+	}
+}
