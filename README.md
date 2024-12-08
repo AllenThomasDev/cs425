db62ff8e280c77d1aec3984ec2a08f5dc2cedcf6
mschmitt-17 <mds17@illinois.edu>
2024-12-08 13:59:39 -0600

    modify readme and add scripts

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # cs425-mp3
 
-### send the code to VMs
+### send the code and sample executables to VMs
 
 1. `chmod +x deploy.sh`
 2. `./deploy.sh <net-id>`
+3. `chmod +x deploy_execs.sh`
+4. `./deploy_execs.sh <net-id>`
 
 ### Running the Client
 
 1. `./start_vms.sh <net-id>`
 2. This will open 10 tmux panes, from where you can start interacting with the daemon
 3. ./daemon to start the daemon on the VM
+4. RainStorm <op1_exe> <op1_type> <op1_arg> <op2_exe> <op2_type> <op2_arg> <HyDFS_src> <HyDFS_dest> <num_tasks> to run RainStorm
---
+++ scripts/deploy_execs.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "No argument supplied. Please provide a net-id."
+  exit 1
+fi
+
+VMDomainNames=(
+  "fa24-cs425-5401.cs.illinois.edu"
+  "fa24-cs425-5402.cs.illinois.edu"
+  "fa24-cs425-5403.cs.illinois.edu"
+  "fa24-cs425-5404.cs.illinois.edu"
+  "fa24-cs425-5405.cs.illinois.edu"
+  "fa24-cs425-5406.cs.illinois.edu"
+  "fa24-cs425-5407.cs.illinois.edu"
+  "fa24-cs425-5408.cs.illinois.edu"
+  "fa24-cs425-5409.cs.illinois.edu"
+  "fa24-cs425-5410.cs.illinois.edu"
+)
+
+REMOTE_DIR="~"
+USER=$1
+
+# Function to copy the executable to a remote server
+copy_executable() {
+  local domain_name=$1
+  echo "Copying files to $domain_name..."
+  for file in ../operators/*; do
+    scp "$file" "$USER@$domain_name:$REMOTE_DIR"
+  done
+  echo "files copied to $domain_name"
+}
+
+# Iterate over each VM and copy the executable
+for domain_name in "${VMDomainNames[@]}"; do
+  echo "Processing $domain_name..."
+  ssh "$USER@$domain_name" "mkdir -p $REMOTE_DIR"
+  copy_executable "$domain_name"
+  echo "Finished processing $domain_name"
+  echo "------------------------"
+done
+
+echo "Script execution completed"
